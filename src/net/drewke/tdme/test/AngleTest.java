0f2119c3110b395421d4dc0ed8a054628772af7e
Andreas Drewke <andreas@drewke.net>
2016-09-03 19:12:13 +0200

    Tests::added AngleTest, need something console using for my first C++ port tests

---
+++ src/net/drewke/tdme/test/AngleTest.java
@@ -0,0 +1,18 @@
+package net.drewke.tdme.test;
+
+import net.drewke.tdme.math.Vector3;
+
+public class AngleTest {
+
+	public AngleTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		Vector3 a = new Vector3(0f,0f,1f).normalize();
+		Vector3 b = new Vector3(-1f,0f,+1f).normalize();
+		Vector3 n = new Vector3(0f,1f,0f);
+		System.out.println(Vector3.computeAngle(a, b, n));
+	}
+
+}
