985131c06ee51d2cf17fb045ecb4f04659337400
Andreas Drewke <andreas@drewke.net>
2017-03-31 21:52:06 +0200

    Engine: - some JAVADOC love

--- src/net/drewke/tdme/engine/Engine.java
+++ src/net/drewke/tdme/engine/Engine.java
@@ -945,7 +945,7 @@ public final class Engine {
 	 * Retrieves object by mouse position
 	 * @param mouse x
 	 * @param mouse y
-	 * @param filter
+	 * @return entity or null
 	 */
 	public Entity getObjectByMousePosition(int mouseX, int mouseY) {
 		return getObjectByMousePosition(mouseX, mouseY, null);
@@ -956,6 +956,7 @@ public final class Engine {
 	 * @param mouse x
 	 * @param mouse y
 	 * @param filter
+	 * @return entity or null
 	 */
 	public Entity getObjectByMousePosition(int mouseX, int mouseY, EntityPickingFilter filter) {
 		// get world coordinate
